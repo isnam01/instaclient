db0ddad9abb4f18fdd8a14bd516013fe7b57b4c1
isnam01 <guptamansi199901@gmail.com>
2020-12-19 18:31:30 +0530

    debugged

--- src/components/pages/myposts.js
+++ src/components/pages/myposts.js
@@ -191,77 +191,81 @@ const deletePost=(id)=>{
             {
                 state && data ?
                 (
-                    
+                
                 data.map((item)=>{
                     return(
     
                         <div className="card home-card" key={item._id}>
                             <div className="head">
-                            {console.log((state))}
-                            <h5><Link to={item.postedby._id!==state._id? "/profile/"+item.postedby._id :"/profile"} style={{fontSize:"19px"}}>{item.postedby.name}</Link></h5>
-                            {
-                                item.postedby._id===state._id ?
-                                <i className="material-icons" style={{float:"right",fontSize:"18px" ,color:"#282828"}} onClick={()=>{deletePost(item._id)}}>delete</i>
-                                :
-                                null
-                            }
-                        </div>
-                        
-                        <div className="card-image">
-                            <img src={item.picture} alt="pic" ></img>
-                        </div>
-                        <div className="card-content">
-                            <div style={{display:"flex",justifyContent:"flex-start",fontSize:"15px"}}>
-                                {                           
-                                    item.likes.includes(state._id) ?
-                                    <i className="material-icons" style={{color:"red"}} onClick={()=>{unlikepost(item._id)}}>favorite</i>
+                                {console.log((state))}
+                                <h5><Link to={item.postedby._id!==state._id? "/profile/"+item.postedby._id :"/profile"} style={{fontSize:"19px"}}>{item.postedby.name}</Link></h5>
+                                {
+                                    item.postedby._id===state._id ?
+                                    <i className="material-icons" style={{float:"right",fontSize:"18px" ,color:"#282828"}} onClick={()=>{deletePost(item._id)}}>delete</i>
                                     :
-                                    <i className="material-icons" style={{color:"red"}} onClick={()=>{likepost(item._id)}}>favorite_border</i>
+                                    null
                                 }
-                                    <h7 style={{margin:"5px"}}>{item.likes.length} likes</h7>
                             </div>
-                            <h5 style={{fontWeight:"900",fontSize:"17px" }}>{item.title}</h5>
+                        
+                            <div className="card-image">
+                                <img src={item.picture} alt="pic" ></img>
+                            </div>
+                            <div className="card-content">
+                                <div style={{display:"flex",justifyContent:"flex-start",fontSize:"15px"}}>
+                                    {                           
+                                        item.likes.includes(state._id) ?
+                                        <i className="material-icons" style={{color:"red"}} onClick={()=>{unlikepost(item._id)}}>favorite</i>
+                                        :
+                                        <i className="material-icons" style={{color:"red"}} onClick={()=>{likepost(item._id)}}>favorite_border</i>
+                                    }
+                                        <h7 style={{margin:"5px"}}>{item.likes.length} likes</h7>
+                                </div>
+                                <h5 style={{fontWeight:"900",fontSize:"17px" }}>{item.title}</h5>
                             
-                            <div style={{paddingTop:"15px"}}>
-                            <p style={{fontWeight:"900",fontSize:"15px"}}>{item.comments.length} Comments</p>
-                            <div style={{backgroundColor:"#F0F0F0",borderRadius:"5px",padding:"0 6px",margin:"3px"}}>
-                            {
-                                item.comments.map((record)=>{
-                                    return(
-                                    <div key={record._id+'b'} style={{display:"flex",justifyContent:"space-between",paddingTop:"5px"}}>
-                                        <div style={{display:"flex"}}>
-                                            <span key={record._id+'a'} style={{fontWeight:"900",fontSize:"14px" }}><b>{record.postedby.name+":"}</b></span>
-                                            <span key={record._id} style={{fontSize:"14px",marginLeft:"4px", color:"#505050"}}> {record.text}</span>
-                                        </div>
+                                <div style={{paddingTop:"15px"}}>
+                                    <p style={{fontWeight:"900",fontSize:"15px"}}>{item.comments.length} Comments</p>
+                                    <div style={{backgroundColor:"#F0F0F0",borderRadius:"5px",padding:"0 6px",margin:"3px"}}>
                                         {
-                                            record.postedby._id===state._id ?
-                                                <i className="material-icons" style={{fontSize:"14px"}} onClick={()=>deleteComment(record._id,item._id)}>delete</i>
-                                                :
-                                                null
+                                        item.comments.map((record)=>{
+                                            return(
+                                            <div key={record._id+'b'} style={{display:"flex",justifyContent:"space-between",paddingTop:"5px"}}>
+                                                <div style={{display:"flex"}}>
+                                                    <span key={record._id+'a'} style={{fontWeight:"900",fontSize:"14px" }}><b>{record.postedby.name+":"}</b></span>
+                                                    <span key={record._id} style={{fontSize:"14px",marginLeft:"4px", color:"#505050"}}> {record.text}</span>
+                                                </div>
+                                                {
+                                                    record.postedby._id===state._id ?
+                                                        <i className="material-icons" style={{fontSize:"14px"}} onClick={()=>deleteComment(record._id,item._id)}>delete</i>
+                                                        :
+                                                        null
+                                                }
+                                            </div>
+                                            )
+                                        })
                                         }
                                     </div>
-                                    )
-                                })
-                            }
-                            </div>
-                            </div>
+                                </div>
             
-                            <form onSubmit={(e)=>{
-                                e.preventDefault()
-                               makeComment(e.target[0].value,item._id)
-                               e.target.reset();
-                            }} style={{backgroundColor:"#F0F0F0",borderRadius:"5px",margin:"3px",marginTop:"10px",padding:"0 3px"}}>
-                                <input type="text" placeholder="Add a comment" style={{fontSize:"13px",padding:"0",margin:"0",height:"26px"}} ></input>
-                            </form>
+                                <form onSubmit={(e)=>{
+                                    e.preventDefault()
+                                makeComment(e.target[0].value,item._id)
+                                e.target.reset();
+                                }} style={{backgroundColor:"#F0F0F0",borderRadius:"5px",margin:"3px",marginTop:"10px",padding:"0 3px"}}>
+                                    <input type="text" placeholder="Add a comment" style={{fontSize:"13px",padding:"0",margin:"0",height:"26px"}} ></input>
+                                </form>
                             
+                            </div>
+                        
                         </div>
-                    </div>
+                    
                     )
+
                 })
-                )
-                : null      
                 
-            }   
+                )
+                : null        
+            } 
+            <h5>Follow people to see posts</h5>  
         </div>
     )
 }
